4411fb90103774b05b851dbcbd3747cae42f2dbe
Choijy <shvk54@gmail.com>
2018-12-15 14:42:27 +0900

    My databaseproject app, first commit

---
+++ hufsfood/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ex: /parkinglist/
+    path('', views.index, name='index'),
+
+    path('hufsfood/chicken',views.show_chicken,name = 'chicken'),
+    path('hufsfood/chicken/',views.show_chicken,name = 'chicken'),
+
+    path('hufsfood/chinese', views.show_chinese, name='chinese'),
+    path('hufsfood/chinese/', views.show_chinese, name='chinese'),
+
+    path('hufsfood/pizza', views.show_pizza, name='pizza'),
+    path('hufsfood/pizza/', views.show_pizza, name='pizza'),
+
+    path('hufsfood/korean', views.show_korean, name='korean'),
+    path('hufsfood/korean/', views.show_korean, name='korean'),
+
+    path('hufsfood/snack', views.show_snack, name='snack'),
+    path('hufsfood/snack/', views.show_snack, name='snack'),
+
+    path('hufsfood/cafe', views.show_cafe, name='cafe'),
+    path('hufsfood/cafe/', views.show_cafe, name='cafe'),
+
+    path('hufsfood/bossam', views.show_bossam, name='bossam'),
+    path('hufsfood/bossam/', views.show_bossam, name='bossam'),
+
+    path('hufsfood/night', views.show_night, name='night'),
+    path('hufsfood/night/', views.show_night, name='night'),
+
+    path('hufsfood/japanese', views.show_japanese, name='japanese'),
+    path('hufsfood/japanese/', views.show_japanese, name='japanese'),
+    ]
---
+++ hufsfood/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Restaurant
+
+
+#테이블이름을 restaurant라고 가정
+
+def index(request, show='default'):
+    return render(request, 'hufsfood/init.html')
+
+def show_chicken(request):
+    restaurants = Restaurant.objects.filter(kind='치킨')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_chinese(request):
+    restaurants = Restaurant.objects.filter(kind='중국집')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_pizza(request):
+    restaurants = Restaurant.objects.filter(kind='양식')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_korean(request):
+    restaurants = Restaurant.objects.filter(kind='한식')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_cafe(request):
+    restaurants = Restaurant.objects.filter(kind='카페')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_snack(request):
+    restaurants = Restaurant.objects.filter(kind='분식')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_night(request):
+    restaurants = Restaurant.objects.filter(kind='야식')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_bossam(request):
+    restaurants = Restaurant.objects.filter(kind='족발/보쌈')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
+def show_japanese(request):
+    restaurants = Restaurant.objects.filter(kind='일식')
+    context = {'restaurants':restaurants}
+    return render(request, 'hufsfood/result.html', context)
+
